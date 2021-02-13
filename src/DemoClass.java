84f1820286f9d43f55d9ded1c95e7841f7c403ee
muzaffarraja <muzaffarraja@gmail.com>
2021-02-13 18:29:38 -0500

    Update DemoClass.java

--- src/DemoClass.java
+++ src/DemoClass.java
@@ -4,6 +4,10 @@ public class DemoClass {
 	public static void main(String[] args) {
 		
 		System.out.println("This is Just of testing  ");
+		
+		System.out.println("This is scond print statement")
+		System.out.println("This is third print statement")
+		System.out.println("This is forth print statement")
 
 	}
 
