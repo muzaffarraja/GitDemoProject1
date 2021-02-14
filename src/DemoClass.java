f201c2e4eabe737f34b12803887bd32634bdedcc
muzaf <muzaf@DESKTOP-LOMDBBG.home>
2021-02-14 16:08:50 -0500

    add display method

--- src/DemoClass.java
+++ src/DemoClass.java
@@ -2,21 +2,12 @@
 public class DemoClass {
 
 	public static void main(String[] args) {
+		display();
+	}	
 		
-		System.out.println("This is Just of testing  ");
+		public static void display(){
+		System.out.println("Muzaffar Raja");
 		
-		System.out.println("This is scond print statement");
-		System.out.println("This is third print statement");
-		System.out.println("This is forth print statement");
-		System.out.println("This is Fifth print statement");
-		System.out.println("This is sixth print statement");
-		System.out.println("This is seventh print statement");
-	
-	
-		System.out.println("This is 8th print statement");
-		System.out.println("This is 9th print statement");
-		System.out.println("This is 10 print statement");
-	
 	}
-
+	
 }
